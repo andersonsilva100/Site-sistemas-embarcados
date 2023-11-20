dad79e2aa23e233dca4906aa74f8d1ab2f16fe67
andersonsilva100 <engenhariaembarcada@gmail.com>
2023-11-20 20:16:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//Método que aguarda o documento HTML ser carregado evitando Erros
+document.addEventListener('DOMContentLoaded', function (){
+var home=document.getElementById("home");  //Acessa o elemento pelo seu id
+var home=document.getElementById("sobre");
+var home=document.getElementById("projetos");
+var home=document.getElementById("contatos");
+
+
+});
+
