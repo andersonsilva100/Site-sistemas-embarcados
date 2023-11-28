226bb01e8e890b0fc556b1519b45379cc2d24c0f
andersonsilva100 <engenhariaembarcada@gmail.com>
2023-11-28 18:45:34 -0300

    Atualizacao Site Engenharia

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-
-//Método que aguarda o documento HTML ser carregado evitando Erros
-document.addEventListener('DOMContentLoaded', function (){
-var home=document.getElementById("home");  //Acessa o elemento pelo seu id
-var home=document.getElementById("sobre");
-var home=document.getElementById("projetos");
-var home=document.getElementById("contatos");
+const express = require('express');
+const app = express();
 
 
-});
+app.get(4000, function (erro) {
+    if(erro){
+        console.log("ocorreu um erro");
+    }
+    else{
+        console.log("servidor iniciado com sucesso");
+    }
+
+})
+
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    alert("Nome: " + name + "\nE-mail: " + email + "\nMensagem: " + message);
+}
+
+
+
+//var http = required("http")  //carrega o módulo  http na variavel http
+//http.createServer().listen(8181); //criação de um server de http em node
+
+//console.log("meu servidor esta rodando");
+//--------------------------------------------------
+//http.creatServer(function(requisicao,resposta){
+// resposta.end("<h1> Bem vindo ao meu site" );
+//)}.listen(3000);
+//console.log("meu servidor esta rodando");
 
